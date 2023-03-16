b59bbc3b163f69c2647d38c242b05b1cac3143bd
sergiop45 <sergiopsantos33@gmail.com>
2023-03-16 20:43:28 -0300

    update-delete

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path 
-from .views import home, salvar
+from .views import home, salvar, editar, update, deletar
 
 urlpatterns = [
     path('', home),
-    path('salvar/', salvar, name='salvar')
+    path('salvar/', salvar, name='salvar'),
+    path('editar/<int:id>', editar, name='editar'),
+    path('update/<int:id>', update, name='update'),
+    path('deletar/<int:id>', deletar, name='deletar')
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -13,3 +13,23 @@ def salvar(request):
     Cliente.objects.create(nome=nome, sobrenome=sobrenome)
     clientes = Cliente.objects.all()
     return render(request, 'index.html', {'clientes': clientes})
+
+def editar(request, id):
+    cliente = Cliente.objects.get(id=id)
+    return render(request, 'update.html', {'cliente': cliente})
+
+def update(request, id):
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    cliente = Cliente .objects.get(id=id)
+    cliente.nome = nome
+    cliente.sobrenome = sobrenome
+    cliente.save()
+    clientes = Cliente.objects.all()
+    return render(request, 'index.html', {'clientes': clientes})
+
+def deletar(request, id):
+    cliente = Cliente.objects.get(id=id)
+    cliente.delete()
+    clientes = Cliente.objects.all()
+    return render(request, 'index.html', {'clientes': clientes})
